e6875be53ab3de3de98e3f8f003de6c0bb013a69
Ru <rickqaq@gmail.com>
2023-10-04 11:18:44 +0800

    user連接雲端成功

---
+++ android/app/src/main/java/com/example/foodapp_user/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.foodapp_user;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:csv/csv.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:file_picker/file_picker.dart';
+import 'package:googleapis/drive/v3.dart' as drive;
+import 'package:google_sign_in/google_sign_in.dart' as signIn;
+import 'package:http/http.dart' as http;
+import 'package:googleapis_auth/googleapis_auth.dart' as auth;
+import 'package:googleapis/drive/v3.dart' show Media;
+import 'package:file_picker/file_picker.dart';
+import 'package:googleapis_auth/auth_io.dart';
+import 'dart:convert'; // for utf8
+import 'dart:async'; // for Stream
+import 'shopmenu.dart';
+
+/*
+app:foodapp
+package:com.example.foodapp
+Launcher:com.example.foodapp.MainActivity
+SHA1: 83:4D:3C:8A:4C:BB:10:13:48:81:E5:F3:EA:8D:E9:19:1B:0F:CC:B1
+ */
+//增加從雲端抓資料與輸出資料
+
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //返回一個 MaterialApp Widget，該Widget定義了應用程式的主題和首頁
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+
+
+class HomePage extends StatefulWidget {
+  //const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //HomePage 的狀態類別，用於管理狀態變化
+  List<List<dynamic>> _data = [];
+
+  get auth2 => null;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /*
+      appBar: AppBar(
+        title: Text("Blofood"),
+      ),
+      */
+
+        body: ListView(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start, // 將子元素靠左對齊
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    Navigator.push(context , MaterialPageRoute(builder: (context) =>shopmenu()));
+                  },
+                  child: Text("打開店家"),
+                ),
+                /*
+                ElevatedButton(
+                  onPressed: () async {
+                    Navigator.push(context , MaterialPageRoute(builder: (context) =>sign_in()));
+                  },
+                  child: Text("註冊"),
+                ),
+                 */
+              ],
+            ),
+          ],
+        ));
+  }
+}
